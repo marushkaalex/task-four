870e193efbcd21314f9608a3cd87f23a34d6c5ef
Alexander Marushka <marushkaalex@gmail.com>
2016-08-21 22:11:15 +0600

    Use BlockingQueue in ConnectionPool

--- src/main/java/com/epam/am/pool/Runner.java
+++ src/main/java/com/epam/am/pool/Runner.java
@@ -4,13 +4,14 @@ import com.epam.am.pool.dao.DaoException;
 import com.epam.am.pool.dao.DaoFactory;
 import com.epam.am.pool.dao.UserDao;
 import com.epam.am.pool.db.ConnectionPool;
+import com.epam.am.pool.db.ConnectionPoolException;
 import com.epam.am.pool.model.User;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class Runner {
-    public static void main(String[] args) throws DaoException, IOException {
+    public static void main(String[] args) throws DaoException, IOException, ConnectionPoolException {
         Properties properties = new Properties();
         properties.load(Runner.class.getClassLoader().getResourceAsStream("pool.properties"));
         ConnectionPool.init(properties);
--- src/main/java/com/epam/am/pool/dao/jdbc/JdbcDaoFactory.java
+++ src/main/java/com/epam/am/pool/dao/jdbc/JdbcDaoFactory.java
@@ -1,13 +1,23 @@
 package com.epam.am.pool.dao.jdbc;
 
+import com.epam.am.pool.dao.DaoException;
 import com.epam.am.pool.dao.DaoFactory;
 import com.epam.am.pool.dao.UserDao;
 import com.epam.am.pool.db.ConnectionPool;
+import com.epam.am.pool.db.ConnectionPoolException;
 
 import java.sql.Connection;
 
 public class JdbcDaoFactory extends DaoFactory {
-    private Connection connection = ConnectionPool.getInstance().getConnection();
+    private Connection connection;
+
+    public JdbcDaoFactory() throws DaoException {
+        try {
+            this.connection = ConnectionPool.getInstance().getConnection();
+        } catch (ConnectionPoolException e) {
+            throw new DaoException(e);
+        }
+    }
 
     @Override
     public UserDao getUserDao() {
